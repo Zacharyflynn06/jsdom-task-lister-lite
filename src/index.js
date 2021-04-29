c4963e8567b96a1c89ddf95ddcb5a299adfaa195
Zac <zacharyflynn06@gmail.com>
2021-04-29 15:08:27 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const newTaskForm = document.querySelector('#create-task-form');
+  const newTaskDescription = document.querySelector("#new-task-description");
+
+  const newTaskUl = document.querySelector("#tasks");
+
+  newTaskForm.addEventListener("submit", createNewTask)
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.querySelector("#new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
